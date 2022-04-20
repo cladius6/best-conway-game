2de282be0aa2b0475596fc3ad0dbe1ea137b94b4
Klaudiusz <d1f1.6@protonmail.com>
2022-04-20 11:01:52 +0200

    BoardModule imported in app module

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
