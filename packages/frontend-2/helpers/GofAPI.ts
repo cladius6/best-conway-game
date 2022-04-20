241d53510a1eaa7b414bb7e6e9729984ac020ca3
Krystian Kat <krystian.kat@protonmail.com>
2022-04-20 21:58:09 +0200

    refactor: change class methods to static

--- packages/frontend-2/helpers/GofAPI.ts
+++ packages/frontend-2/helpers/GofAPI.ts
@@ -16,9 +16,9 @@ export type INumberOfNeighbors = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type IBoard = ICellState[][];
 
 export class GofAPI {
-  private localUrl = 'http://localhost:3333/api/board';
+  static localUrl = 'http://localhost:3333/api/board';
 
-  private postJson(url: string, method: string, data: any) {
+  static postJson(url: string, method: string, data: any) {
     return fetch(url, {
       body: JSON.stringify(data),
       method: method,
@@ -28,12 +28,19 @@ export class GofAPI {
     });
   }
 
-  public async getBoard() {
+  static async getBoard() {
     const response = await fetch(this.localUrl);
-    return response.json();
+    const data = await response.json();
+    return data;
   }
 
-  public async resizeBoard(body: { size: number }) {
+  static async tick() {
+    const response = await fetch(`${this.localUrl}/tick`);
+    const data = await response.json();
+    return data;
+  }
+
+  static async resizeBoard(body: { size: number }) {
     const response = await this.postJson(
       `${this.localUrl}/resize`,
       'POST',
@@ -42,7 +49,7 @@ export class GofAPI {
     return response.json();
   }
 
-  public async setAliveCell(body: { row: number; col: number }) {
+  static async setAliveCell(body: { row: number; col: number }) {
     const response = await this.postJson(
       `${this.localUrl}/resize`,
       'PUT',
@@ -51,19 +58,8 @@ export class GofAPI {
     return response.json();
   }
 
-  public async tick() {
-    const response = await fetch(`${this.localUrl}/tick`);
-    return response.json();
-  }
-
-  public async setBoard(body: { cells: IBoard }) {
+  static async setBoard(body: { cells: IBoard }) {
     const response = await this.postJson(`${this.localUrl}/cells`, 'PUT', body);
     return response.json();
   }
 }
-
-export const getBoard = async () => {
-  const localUrl = 'localhost:3333/api/board';
-  const response = await fetch(localUrl);
-  return response.json();
-};
