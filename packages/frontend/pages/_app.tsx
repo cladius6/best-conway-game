2d79147b819186bba2fb1be20b11fa6b86dd63fc
hub33k <hub33k@yahoo.com>
2022-04-20 13:27:43 +0200

    refactor: cleanup bailerplate code

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to frontend!</title>
+        <title>Best conway game</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
