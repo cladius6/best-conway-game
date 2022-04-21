9c5e229354c5576e10a356adbe0f157eb5f7155a
Klaudiusz <d1f1.6@protonmail.com>
2022-04-21 15:31:26 +0200

    ci

--- packages/backend/src/app/board/dto/set-cell.dto.ts
+++ packages/backend/src/app/board/dto/set-cell.dto.ts
@@ -1,6 +1,7 @@
 import { IsInt, Min } from 'class-validator';
 import { ISetCell } from '@conway-game/interfaces';
 
+// TODO" 1
 export class SetCellDto implements ISetCell {
   @IsInt()
   @Min(0)
