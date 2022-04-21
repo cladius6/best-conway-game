112f33cf9e3d7d0d78ba2a6c8d6f14af069b01ed
Klaudiusz <d1f1.6@protonmail.com>
2022-04-21 15:38:39 +0200

    ci

--- packages/backend/src/app/board/dto/resize-board.dto.ts
+++ packages/backend/src/app/board/dto/resize-board.dto.ts
@@ -1,6 +1,7 @@
 import {IsInt, IsPositive} from "class-validator";
 import {IBoardResize} from "@conway-game/interfaces";
 
+//: t
 export class ResizeBoardDto implements IBoardResize{
   @IsInt()
   @IsPositive()
